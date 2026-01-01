715a0c8e14726f734d0a4653f7b2b3f3dc2904b5
pragati kumawat <pragatikumawat2141@gmail.com>
2026-01-01 16:56:07 +0530

    edit port

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ if (process.env.NODE_ENV !== 'production') {
 }
 
 const app = express();
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 10000;
 
 // --- 1. CORS Configuration (CRITICAL FOR DEPLOYMENT) ---
 // In a production environment (like Render), we use the environment variables 
